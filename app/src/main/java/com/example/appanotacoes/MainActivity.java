7456885b15cd1613b75d3f091d6ee7135fdfc0a0
Lucca465 <lucca465@gmail.com>
2021-10-27 12:09:21 -0300

    Criando a logica

--- app/src/main/java/com/example/appanotacoes/MainActivity.java
+++ app/src/main/java/com/example/appanotacoes/MainActivity.java
@@ -7,26 +7,52 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.appanotacoes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // floating action button
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        editAnotacao = findViewById(R.id.editAnotacao);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // Validar se foi digitado algo
+                String textoRecuperado = editAnotacao.getText().toString();
+                if(textoRecuperado.equals("")){
+
+                    Snackbar.make(view, "Preencha a anotação!", Snackbar.LENGTH_LONG).show();
+
+                }else {
+
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação salva com sucesso!", Snackbar.LENGTH_LONG).show();
+
+                }
             }
         });
+
+        // Recuperar as anotacao
+        String anotacao = preferencias.recuperarAnotacao();
+        if(!anotacao.equals("")){
+            editAnotacao.setText(anotacao);
+        }
     }
 }
